53064b3e217beb3b3a2381bcb13fb73802e55d40
Alexa Amoriello (RIT Student) <axa9517@rit.edu>
2023-04-05 17:48:50 -0400

    buttons update with cookies
    aa

--- Chronoquartz/Assets/Scripts/UIManager.cs
+++ Chronoquartz/Assets/Scripts/UIManager.cs
@@ -47,7 +47,6 @@ public class UIManager : MonoBehaviour
         ingredients.Add("flour");
         ingredients.Add("sugar");
         ingredients.Add("oatmeal");
-        
 
         foreach (GameObject window in windows)
         {
